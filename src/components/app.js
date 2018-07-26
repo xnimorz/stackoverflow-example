93712807c3cb81b91a3ee73b8af9f483ba7ba25d
Nikita Mostovoy <nik.mostovoy@gmail.com>
2018-07-26 10:50:55 +0300

    51517890 demo

---
+++ src/components/app.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return <div>React simple starter</div>;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from "./components/app";
+import reducers from "./reducers";
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>,
+  document.querySelector(".container")
+);
